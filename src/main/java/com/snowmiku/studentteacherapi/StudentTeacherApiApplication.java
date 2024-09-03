e91f615150590fe0d96ce6b598490151087f3a6a
Alan Zhou <4lanzhou@gmail.com>
2024-09-03 03:09:40 -0700

    init

---
+++ src/main/java/com/snowmiku/studentteacherapi/StudentTeacherApiApplication.java
@@ -0,0 +1,13 @@
+package com.snowmiku.studentteacherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTeacherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentTeacherApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snowmiku/studentteacherapi/StudentTeacherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowmiku.studentteacherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTeacherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
